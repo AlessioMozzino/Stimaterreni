63ead97b047512744612a0c5fd2a80e3d45f2e71
AlessioMozzino <82084197+AlessioMozzino@users.noreply.github.com>
2024-03-03 16:43:20 +0100

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,46 @@
+<!-- Inizio informazioni fiscali -->
+<div class="sub-container-footer">
+    <h4 class="credits-footer">
+        stimaterreni.it è un progetto ideato e gestito da Alessio Mozzino. Tutti i diritti sono riservati.<br>
+        <span class="title-credits-footer">Sede legale</span>Piazza Vittorio Veneto, 2<br>17027, Giustenice (SV)<br>
+        <span class="title-credits-footer">Sede operativa</span>Via Giacomo Matteotti, 173<br>17022, Borgio Verezzi (SV)<br><br>
+        Partita IVA 01782640096<br>
+        E-mail <a href="mailto:info@stimaterreni.it">info@stimaterreni.it</a>
+    </h4>
+</div>
+<!-- Fine informazioni fiscali -->
+
+<!-- Inizio informazioni legali -->
+<div class="sub-container-footer">
+    <h4 class="credits-footer">
+        <ul>
+            <li>Area legale</li>
+            <li><a href="index.php?page=privacy">Politica sulla privacy</a></li>
+            <li><a href="index.php?page=cookie">Politica sui cookies</a></li>
+            <li><a href="index.php?page=condizioni_vendita">Termini e condizioni</a></li>
+        </ul>
+    </h4>
+</div>
+<!-- Fine informazioni legali -->
+
+<!-- Inizio informazioni contabili -->
+<div class="sub-container-footer">
+    <h4 class="credits-footer">
+        <ul>
+            <li>Area contabile</li>
+            <li><a href="index.php?page=pagamenti">Pagamenti</a></li>
+        </ul>
+    </h4>
+</div>
+<!-- Fine informazioni contabili -->
+
+<!-- Inizio link utili -->
+<div class="sub-container-footer">
+    <h4 class="credits-footer">
+        <ul>
+            <li>Link utili</li>
+            <li><a href="https://www.agenziaentrate.gov.it/portale/web/guest/schede/fabbricatiterreni/omi/banche-dati/valori-agricoli-medi" target="_blank">Valori agricoli medi</a></li>
+        </ul>
+    </h4>
+</div>
+<!-- Fine link utili -->
---
+++ template/header.php
@@ -0,0 +1,11 @@
+<!-- Inizio contenitore logo -->
+<div class="sub-container-50" id="header-logo">
+    <h3 id="h3-header-logo"><a href="index.php?pag=home">stimaterreni.it</a></h3>
+</div>
+<!-- Fine contenitore logo -->
+
+<!-- Inizio contenitore menu -->
+<div class="sub-container-50" id="header-menu-btn">
+    <img id="img-header-menu-btn" src="https://img.icons8.com/ios-glyphs/30/000000/menu--v1.png" alt="menu"/>
+</div>
+<!-- Fine contenitore menu --> 
