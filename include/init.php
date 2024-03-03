e5d9928b6565a588be5616a4d251ffc8664622e0
AlessioMozzino <82084197+AlessioMozzino@users.noreply.github.com>
2024-03-03 16:42:04 +0100

    Add files via upload

---
+++ include/init.php
@@ -0,0 +1,10 @@
+<?php
+    /* File di centralizzazione delle inclusioni */
+
+    // Inclusione moduli
+    require_once(__DIR__ . '/module/func.php');
+    require_once(__DIR__ . '/module/var.php');
+
+    // Inclusione file di connessione al database
+    require_once(__DIR__ . '/db/conn.php');
+?>
